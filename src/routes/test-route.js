9c97bc4761dfe729e7e4b34f8c3378914249f745
doror1989 <doror1989@gmail.com>
2023-08-15 17:39:14 +0300

    Update test-route.js

--- src/routes/test-route.js
+++ src/routes/test-route.js
@@ -1,7 +1,15 @@
 import express from 'express';
+const winston = require('winston');
+const logger = require('./logger');
+
+logger.info('Service started.');
 
 
 express.get('/test', function(req, res, next) {
+    var first_name = 'test';
+    var email = 'test@test.com';
+    console.log(first_name);
+    logger.error(email);
     return ProductModel.find(function (err, products) {
         if (!err) {
             res.render('app', {products: products, root: URL_ROOT});
